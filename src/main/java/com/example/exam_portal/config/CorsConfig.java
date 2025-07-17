5167b5edcfcf97203b88b58166df66fccf6307a9
dung11122005 <dungakaishi900@gmail.com>
2025-07-17 22:31:36 +0700

    1

---
+++ src/main/java/com/example/exam_portal/config/CorsConfig.java
@@ -0,0 +1,37 @@
+package com.example.exam_portal.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {// https://stackoverflow.com/a/40431994
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // cho phép các URL nào có thể kết nối tới backend
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
+
+        // các method nào đc kết nối
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE",
+                "OPTIONS")); // Allowed methods
+
+        // các phần header được phép gửi lên
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type",
+                "Accept", "x-no-retry"));
+
+        // gửi kèm cookies hay không
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        // How long the response from a pre-flight request can be cached by clients
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Apply this configuration to all paths
+        return source;
+    }
+}
