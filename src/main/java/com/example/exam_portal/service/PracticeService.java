453eaac6548cbdee01f80ca3b07f187ee2b20d6e
dung11122005 <dungakaishi900@gmail.com>
2025-08-02 00:26:30 +0700

    final-2/8/2025-12h26AM

--- src/main/java/com/example/exam_portal/service/PracticeService.java
+++ src/main/java/com/example/exam_portal/service/PracticeService.java
@@ -15,7 +15,7 @@ public class PracticeService {
     private GeminiClient geminiClient;
 
     public String generateRandomVietnameseSentence() {
-        return geminiClient.ask("Hãy tạo một câu tiếng Việt đơn giản (~20–25 từ) cho học sinh dịch sang tiếng Anh. Chỉ trả về câu tiếng Việt.");
+        return geminiClient.ask("Hãy tạo một đoạn tiếng Việt bất cứ chủ đề gì và những câu bạn trả ra không được lặp lại câu trước (~100–120 từ) cho học sinh dịch sang tiếng Anh. Chỉ trả về câu tiếng Việt.");
     }
 
     public FeedbackResult analyzeStudentTranslation(String sourceVi, String studentEn) {
