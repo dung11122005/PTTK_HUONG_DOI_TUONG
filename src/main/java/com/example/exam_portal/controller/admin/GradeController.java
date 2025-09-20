245ecf83e1e061eb862fe7a26dffbbbaf6b4558e
dung11122005 <dungakaishi900@gmail.com>
2025-09-20 10:06:13 +0700

    grade

--- src/main/java/com/example/exam_portal/controller/admin/GradeController.java
+++ src/main/java/com/example/exam_portal/controller/admin/GradeController.java
@@ -1,8 +1,99 @@
 package com.example.exam_portal.controller.admin;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.exam_portal.domain.Grade;
+import com.example.exam_portal.service.GradeService;
 
 @Controller
 public class GradeController {
+    private final GradeService gradeService;
+
+    public GradeController(GradeService gradeService){
+        this.gradeService=gradeService;
+    }
+
+    @GetMapping("/admin/grade")
+    public String getUserPage(Model model, @RequestParam("page") Optional<String> pageOptional) {
+        int page = 1;
+        try {
+            if (pageOptional.isPresent()) {
+                page = Integer.parseInt(pageOptional.get());
+            } else {
+                page = 1;
+            }
+        } catch (Exception e) {
+
+        }
+        Pageable pageable = PageRequest.of(page - 1, 10);
+        Page<Grade> gr = this.gradeService.getAllGradePagination(pageable);
+        List<Grade> grades = gr.getContent();
+        model.addAttribute("grades", grades);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", gr.getTotalPages());
+        return "admin/grade/show";
+    }
+
+    @GetMapping("/admin/grade/create")
+    public String getCreateAcademicYearPage(Model model) {
+        model.addAttribute("newGrade", new Grade());
+        return "admin/grade/create";
+    }
+
+    @PostMapping("/admin/grade/create")
+    public String createGrade(@ModelAttribute("newAcademicYear") Grade grade) {
+
+        this.gradeService.handleSaveGrade(grade);
+        return "redirect:/admin/grade";
+    }
+
+    @GetMapping("/admin/grade/update/{id}")
+    public String getUpdateGradePage(Model model, @PathVariable long id) {
+        Optional<Grade> grade = this.gradeService.getGradeById(id);
+        model.addAttribute("newGrade", grade.get());
+        return "admin/grade/update";
+    }
+
+    @PostMapping("/admin/grade/update/{id}")
+    public String updateGrade(
+            @PathVariable Long id,
+            @ModelAttribute("newGrade") Grade updatedGrade
+    ) {
+        Optional<Grade> ex = this.gradeService.getGradeById(id);
+        if (ex.isPresent()) {
+            Grade existing = ex.get();
+            existing.setName(updatedGrade.getName());
+            existing.setOrderIndex(updatedGrade.getOrderIndex());
+
+            this.gradeService.handleSaveGrade(existing);
+        }
+        return "redirect:/admin/grade";
+    }
+
+
+    @GetMapping("/admin/grade/delete/{id}")
+    public String getDeleteGradePage(Model model, @PathVariable long id) {
+        model.addAttribute("id", id);
+        return "admin/grade/delete";
+    }
     
+    @PostMapping("/admin/grade/delete")
+    public String postDeleteGrade(@RequestParam("id") Long id) {
+        this.gradeService.deleteGrade(id);
+        return "redirect:/admin/grade";
+    }
+
+
 }
--- src/main/java/com/example/exam_portal/service/GradeService.java
+++ src/main/java/com/example/exam_portal/service/GradeService.java
@@ -1,5 +1,39 @@
 package com.example.exam_portal.service;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.example.exam_portal.domain.Grade;
+import com.example.exam_portal.repository.GradeRepository;
+
+
+@Service
 public class GradeService {
-    
+    private final GradeRepository gradeRepository;
+
+
+    public GradeService(GradeRepository gradeRepository){
+        this.gradeRepository=gradeRepository;
+    }
+
+    public Grade handleSaveGrade(Grade Grade) {
+        Grade year = this.gradeRepository.save(Grade);
+        return year;
+    }
+
+
+    public Optional<Grade> getGradeById(long id) {
+        return this.gradeRepository.findById(id);
+    }
+
+    public void deleteGrade(long id) {
+        this.gradeRepository.deleteById(id);
+    }
+
+    public Page<Grade> getAllGradePagination(Pageable page) {
+        return this.gradeRepository.findAll(page);
+    }
 }
